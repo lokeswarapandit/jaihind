1fb2a12d16484d08d3241eb8f4bcbc9e1d3f6bd7
lokeswarapandit <78587712+lokeswarapandit@users.noreply.github.com>
2021-02-25 18:46:45 +0530

    Update bills.sh

--- bills.sh
+++ bills.sh
@@ -1,2 +1,3 @@
 done now
 still in pending
+[![Build Status](https://dev.azure.com/lokeswarapandit0131/yaml/_apis/build/status/lokeswarapandit.jaihind?branchName=master)](https://dev.azure.com/lokeswarapandit0131/yaml/_build/latest?definitionId=1&branchName=master)
